0e95d23353a0d31e071a197a795f8d8e38b10f69
svikam <svikam1@gmail.com>
2024-06-29 13:19:32 +0300

    add all compon

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,20 @@
+// import s from "./FriendList.module.css";
+import FriendListItem from "../FriendListItem/FriendListItem";
+const FriendList = ({ friends }) => {
+    return (
+        <ul>
+            {friends.map(friend => (
+                <li key={friend.id}>
+                    <FriendListItem
+                        avatar={friend.avatar}
+                        name={friend.name}
+                        isOnline={friend.isOnline}
+                    />
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default FriendList;
+
---
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,12 @@
+// import s from "./FriendListItem.module.css";
+const FriendListItem = ({ avatar, name, isOnline }) => {
+    return (
+        <div>
+            <img src={avatar} alt="Avatar" width="48" />
+            <p>{name}</p>
+            <p>{isOnline ? "Online" : "Offline"}</p>
+        </div>
+    );
+}
+
+export default FriendListItem;
