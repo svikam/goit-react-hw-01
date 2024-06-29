f3c190a002f780b02cb7c5f93bc7067befc06a16
svikam <svikam1@gmail.com>
2024-06-29 13:17:18 +0300

    add all components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,10 @@
 import Profile from "./Profile/Profile";
 import userData from "../userData.json";
+import FriendList from "./FriendList/FriendList";
+import friends from "../friends.json";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
+import transactions from "../transactions.json";
+
 const App = () => {
     return (
         <>
@@ -10,6 +15,8 @@ const App = () => {
                 image={userData.avatar}
                 stats={userData.stats}
             />
+            <FriendList friends={friends} />
+            <TransactionHistory items={transactions} />
         </>
     );
 };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,28 +1,29 @@
-const Profile = () => {
+// import s from "./Profile.module.css";
+const Profile = ({ name, tag, location, image, stats }) => {
     return (
         <div>
             <div>
                 <img
-                src="https://cdn-icons-png.flaticon.com/512/1077/1077012.png"
-                alt="User avatar"
+                    src={image}
+                    alt="User avatar"
                 />
-                <p>Petra Marica</p>
-                <p>@pmarica</p>
-                <p>Salvador, Brasil</p>
+                <p>{name}</p>
+                <p>@{tag}</p>
+                <p>{location}</p>
             </div>
 
             <ul>
                 <li>
                 <span>Followers</span>
-                <span>1000</span>
+                <span>{stats.followers}</span>
                 </li>
                 <li>
                 <span>Views</span>
-                <span>2000</span>
+                <span>{stats.views}</span>
                 </li>
                 <li>
                 <span>Likes</span>
-                <span>3000</span>
+                <span>{stats.likes}</span>
                 </li>
             </ul>
         </div>
